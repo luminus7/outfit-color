cd6e0c793948d4e13e2d53e30a2ec55a69cc0879
SeongchanCho <wordnumperiod@gmail.com>
2022-11-21 21:48:53 +0900

    refactor(detect_pedestrian.py): 보행자 추적 코드 클래스화

--- detect_pedestrian.py
+++ detect_pedestrian.py
@@ -2,35 +2,22 @@ import imutils
 import cv2
 
 from os import path
-input_file = f"{path.dirname(path.abspath(__file__))}\\images\\people_walking.jpg"
 
-# Initializing the HOG person
-# detector
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-# Reading the Image
-image = cv2.imread(input_file)
+class PedestrianDetector:
+    def __init__(self, image):
+        self.__image = image
+        self.__hog = cv2.HOGDescriptor()
+        self.__hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        self.__resize()
 
-# Resizing the Image
-image = imutils.resize(image,
-                       width=min(400, image.shape[1]))
+    def __resize(self):
+        # shape: rsize(height), csize(width)
+        imutils.resize(self.__image, width=min(400, self.__image.shape[1]))
 
-# Detecting all the regions in the
-# Image that has a pedestrians inside it
-(regions, _) = hog.detectMultiScale(image,
-                                    winStride=(4, 4),
-                                    padding=(4, 4),
-                                    scale=1.05)
-
-# Drawing the regions in the Image
-for (x, y, w, h) in regions:
-    cv2.rectangle(image, (x, y),
-                  (x + w, y + h),
-                  (0, 0, 255), 2)
-
-# Showing the output Image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
-
-cv2.destroyAllWindows()
+    def catch_regions(self):
+        (self.__regions, _) = self.__hog.detectMultiScale(self.__image,
+                                                          winStride=(4, 4),
+                                                          padding=(4, 4),
+                                                          scale=1.05)
+        return self.__regions
